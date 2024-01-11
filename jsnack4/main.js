a96c8b346345139b004693de40881aa2d5e0964c
AlessandroCampo <alessandrocampo97@gmail.com>
2024-01-11 15:56:55 +0100

    screen display of jsnack4 resul

--- jsnack4/main.js
+++ jsnack4/main.js
@@ -2,10 +2,12 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
 
-const arrayOfInteg = [23, 45, 12, 78, 56, 34, 89, 17, 41, 63, 92, 30, 5, 72, 8, 50, 19, 67, 3, 94, 27, 61, 38, 74, 14, 82, 49, 10, 96, 31];
+const arrayOfInteg = Array.from({ length: 30 }, () => Math.floor(Math.random() * 100));
+
 
 
 let arrayOfIntegOdds = []
+let allOddsSum = 0
 
 
 
@@ -16,7 +18,6 @@ for (let i = 0; i <= arrayOfInteg.length; i++) {
 
     if (i == (arrayOfInteg.length - 1)) {
         console.log("yo")
-        let allOddsSum = 0
         for (let j = 0; j < arrayOfIntegOdds.length; j++) {
             allOddsSum += Number(arrayOfIntegOdds[j])
             if (j == arrayOfIntegOdds.length - 1) {
@@ -26,4 +27,5 @@ for (let i = 0; i <= arrayOfInteg.length; i++) {
     }
 }
 
+document.writeln(`L'array casuale è ${arrayOfInteg} e la somma dei numeri delle posizioni dispari  è ${allOddsSum}`)
 
